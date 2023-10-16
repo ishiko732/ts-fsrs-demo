935ded7ce7373f65564d65e7a791777598c088f4
ishiko732 <ishiko732@gmail.com>
2023-10-16 21:34:05 +0800

    update ts-fsrs@3.0.2

--- src/lib/card.ts
+++ src/lib/card.ts
@@ -1,4 +1,4 @@
-import { CardPrisma, fsrs} from "ts-fsrs";
+import { CardPrisma, createEmptyCard, fsrs} from "ts-fsrs";
 import { getNoteByNid } from "./note";
 import { fixDate } from "ts-fsrs/dist/help";
 
@@ -12,8 +12,9 @@ export async function schedulerCard(nid:number,now:Date){
     const cardByPrisma=note.card  as unknown as CardPrisma
     const card={
         ...cardByPrisma,
-        due:fixDate(cardByPrisma.due),
-        last_review:cardByPrisma.last_review?fixDate(cardByPrisma.last_review):undefined
+        // state:statePrismaToFSRSState(cardByPrisma.state),
+        // due:fixDate(cardByPrisma.due),
+        last_review:cardByPrisma.last_review?cardByPrisma.last_review:undefined
     }
     return f.repeat(card,new Date())
 }
--- src/vendor/fsrsToPrisma/index.ts
+++ src/vendor/fsrsToPrisma/index.ts
@@ -2,18 +2,22 @@ import { createEmptyCard } from "ts-fsrs";
 import { Card, State } from "@prisma/client";
 
 
-interface CardPrismaUnChecked extends Omit<Card, "cid" | "nid" | "last_review"> {
+interface CardPrismaUnChecked extends Omit<Card, "cid" | "nid" | "last_review"|'state'> {
     cid?: number;
     nid?: number;
     last_review?: Date;
+    state: State;
 }
 
 export function createEmptyCardByPrisma(): CardPrismaUnChecked {
     const card = createEmptyCard();
-
     return {
         ...card,
         state: State.New,
         last_review:undefined
     };
 }
+
+// export function statePrismaToFSRSState(prisma:State):fsrsState{
+//     return fsrsState[prisma as keyof typeof fsrsState]
+// }
