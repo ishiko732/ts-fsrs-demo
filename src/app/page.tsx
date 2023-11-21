09d616cd78829e7473a0855ed0d21f7638ef15a0
ishiko732 <ishiko732@gmail.com>
2023-11-21 21:04:27 +0800

    Fix/button not redirecting when clicked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center p-24">
-     <button className='btn btn-outline mx-4'><Link href={'/note'} >Go to Notes</Link></button>
-     <button className='btn btn-outline mx-4'><Link href={'/card'} >Go to Review</Link></button>
+     <Link href={'/note'} ><button className='btn btn-outline mx-4'>Go to Notes</button></Link>
+     <Link href={'/card'} ><button className='btn btn-outline mx-4'>Go to Review</button></Link>
     </main>
   )
 }
