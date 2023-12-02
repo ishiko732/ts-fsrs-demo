4db12bdbc71d1c5fc939e5ed3ed7640463532767
ishiko732 <ishiko732@gmail.com>
2023-12-02 15:04:34 +0800

    Feat/support server compoents

--- src/components/menu/items/index.tsx
+++ src/components/menu/items/index.tsx
@@ -5,17 +5,50 @@ type Props = {
   tip?: string;
   className?: string;
   children: React.ReactNode;
-  onClick?: ((e: React.MouseEvent<HTMLElement, MouseEvent>) => void)|(()=>void);
+  onClick?:
+    | ((e: React.MouseEvent<HTMLElement, MouseEvent>) => void)
+    | (() => void);
+  formAction?: (formData: FormData) => void;
   dialog?: React.ReactNode;
 };
 
-export default function MenuItem({
+export default async function MenuItem({
+  tip,
+  className,
+  children,
+  onClick,
+  formAction,
+  dialog,
+}: Props) {
+  return formAction && !onClick ? (
+    <form action={formAction}>
+      <MenuItemContent
+        tip={tip}
+        className={className}
+        onClick={onClick}
+        dialog={dialog}
+      >
+        {children}
+      </MenuItemContent>
+    </form>
+  ) : (
+    <MenuItemContent
+      tip={tip}
+      className={className}
+      onClick={onClick}
+      dialog={dialog}
+    >
+      {children}
+    </MenuItemContent>
+  );
+}
+async function MenuItemContent({
   tip,
   className,
   children,
   onClick,
   dialog,
-}: Props) {
+}: Partial<Props>) {
   return (
     <>
       <li onClick={onClick} className="max-w-[54px] max-h-10">
