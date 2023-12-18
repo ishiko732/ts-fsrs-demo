9d35dd6176895627d1826bc8e1ad99d086e5cef1
ishiko732 <ishiko732@gmail.com>
2023-12-18 23:37:05 +0800

    Fix/add note api

--- src/app/api/note/route.ts
+++ src/app/api/note/route.ts
@@ -9,8 +9,8 @@ interface Node {
 
 export async function POST(request: NextRequest) {
   const json: Partial<NodeData> = await request.json();
-  if (!json.意味 || !json.英単語) {
-    return NextResponse.json("意味/英単語 invaild data", { status: 400 });
+  if (!json.answer || !json.answer) {
+    return NextResponse.json("question/answer invaild data", { status: 400 });
   }
   const ret = await addNote(json);
   return NextResponse.json({ count: 1 }, { status: 200 });
