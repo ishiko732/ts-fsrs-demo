02b318c76d2daa52194fb5ae614aa7b37ca273fc
ishiko732 <ishiko732@gmail.com>
2023-12-19 21:44:04 +0800

    chore/support vercel prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,7 +5,7 @@ const prisma =
   new PrismaClient({
     datasources: {
       db: {
-        url: `${process.env.DATABASE_URL}?connection_limit=40&pool_timeout=0`,
+        url: `${process.env.DATABASE_URL}?pool_timeout=0`,
       },
     },
     log: [
