082a98dbea02136739cd1a3769d5b1d6ec6c3871
ishiko732 <ishiko732@gmail.com>
2024-01-17 12:44:14 +0800

    Remove StatusBar open/close logic

--- src/components/schedule/StatusBar.tsx
+++ src/components/schedule/StatusBar.tsx
@@ -1,24 +1,12 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import { useCardContext } from "@/context/CardContext";
 import { State } from "ts-fsrs";
-import type { StateBox } from "@/types";
-const boxes: StateBox[] = [State.New, State.Learning, State.Review];
 
 export default function StatusBar() {
   const { noteBox, currentType } = useCardContext();
-  const [stop, setStop] = React.useState(false);
 
-  useEffect(() => {
-    const r = boxes.reduce((sum: number, cur: StateBox) => {
-      return noteBox[cur].length;
-    }, 0);
-    if (r === boxes.length) {
-      setStop(true);
-    }
-  }, [noteBox]);
-
-  return stop? null: (
+  return (
     <div className="flex justify-center text-white">
       <div className={"badge badge-info gap-2 m-1 p-4 text-white"}>
         {currentType === State.New ? (
