84415afe5140e3dc8327a1a8bc813f10c3ad9807
ishiko732 <ishiko732@gmail.com>
2024-01-22 10:15:47 +0800

    typo/lingq type.d.ts

---
+++ src/vendor/lingq/type.d.ts
@@ -0,0 +1,111 @@
+type languageCode =
+    | "cs"
+    | "no"
+    | "tr"
+    | "fi"
+    | "he"
+    | "ro"
+    | "nl"
+    | "el"
+    | "pl"
+    | "eo"
+    | "la"
+    | "da"
+    | "uk"
+    | "sk"
+    | "ms"
+    | "id"
+    | "zh-t"
+    | "hk"
+    | "gu"
+    | "bg"
+    | "fa"
+    | "be"
+    | "ar"
+    | "srp"
+    | "hrv"
+    | "hu"
+    | "ca"
+    | "en"
+    | "fr"
+    | "de"
+    | "es"
+    | "it"
+    | "ja"
+    | "ko"
+    | "zh"
+    | "pt"
+    | "ru"
+    | "sv";
+
+type Context = {
+    id: number,
+    url: string,
+    code: languageCode,
+    title: string,
+    supported: boolean,
+    knownWords: number,
+    lastUsed: string,
+    grammarResourceSlug: string,
+    scheduledForDeletion: boolean,
+}
+
+
+type Lingqs = {
+    count: number,
+    next: string | null,
+    previous: string | null,
+    results: Lingq[]
+}
+
+enum LingqStatus {
+    Rating1 = 0,
+    Rating2 = 1,
+    Rating3 = 2,
+    Rating4 = 3,
+}
+
+enum LingqExtendedStatus {
+    Learning = 0,
+    Known = 3,
+}
+type LingqHint = {
+    id: string,
+    locale: string,
+    text: string,
+    term: string,
+    popularity: number,
+    is_google_translate: boolean,
+    flagged: boolean,
+}
+
+type LingqTransliteration = {
+    [key: string]: string[] | { [key: string]: string }[]
+}
+
+type Lingq = {
+    pk: number,
+    url: string,
+    term: string,
+    fragment: string,
+    importance: number,
+    status: LingqStatus,
+    extended_status: LingqExtendedStatus,
+    last_reviewed_correct: string | null,
+    srs_due_date: string,
+    notes: string | null,
+    audio: string | null,
+    words: string[],
+    tags: string[],
+    hints: LingqHint[]
+    transliteration: LingqTransliteration,
+    gTags: string[],
+    wordTags: string[],
+    readings: { [key: string]: string[] },
+    writings: string[]
+}
+
+type LingqChangeStatus = {
+    status: LingqStatus,
+    extended_status: LingqExtendedStatus,
+}
