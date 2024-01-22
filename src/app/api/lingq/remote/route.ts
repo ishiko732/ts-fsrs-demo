66c10696ff070d79545ad140b7b84cc5693016ca
ishiko732 <ishiko732@gmail.com>
2024-01-22 11:40:21 +0800

    Test/add remote lingq api

---
+++ src/app/api/lingq/remote/route.ts
@@ -0,0 +1,21 @@
+import { request as LingqRequest } from "@/vendor/lingq/request";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function POST(request: NextRequest) {
+    const url = new URL(request.url);
+    const remoteURL = url.searchParams.get("url");
+    const remoteMethod = url.searchParams.get("method");
+    const token = request.headers.get("authorization");
+    if (!token) {
+        return NextResponse.json({ error: "token not found" }, { status: 401 });
+    }
+    if (!remoteURL) {
+        return NextResponse.json({ error: "url not found" }, { status: 400 });
+    }
+    if (!remoteMethod) {
+        return NextResponse.json({ error: "method not found" }, { status: 400 });
+    }
+    const data = await LingqRequest(remoteURL, token, { method: remoteMethod, body: JSON.stringify(request.body) })
+    return NextResponse.json(data)
+}
--- src/app/api/lingq/route.ts
+++
@@ -1,8 +0,0 @@
-import { getLingqContext } from "@/vendor/lingq/request";
-import { NextRequest, NextResponse } from "next/server";
-
-
-export async function GET(request: NextRequest){
-    const data = await getLingqContext({language: "ja", token: request.headers.get("Authorization")??""})
-    return NextResponse.json(data)
-}
