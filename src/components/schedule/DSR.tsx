c55f7a54792da440ae0319b5946849eeb0406f17
ishiko <ishiko732@gmail.com>
2024-03-03 16:20:12 +0800

    Fix/DSR display

--- src/components/schedule/DSR.tsx
+++ src/components/schedule/DSR.tsx
@@ -1,9 +1,10 @@
 'use client'
 import { useCardContext } from "@/context/CardContext"
+import { State } from "ts-fsrs";
 
 export default function DSRDisplay() {
-    const { DSR, open } = useCardContext();
-    return DSR && !open ? (
+    const { DSR, open, currentType } = useCardContext();
+    return DSR && !open && currentType === State.Review ? (
         <div className="flex justify-center opacity-15 flex-col text-left mx-auto">
             <div>{`D : ${DSR.D.toFixed(2)}`}</div>
             <div>{`S : ${DSR.S.toFixed(2)}`}</div>
