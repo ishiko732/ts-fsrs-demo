2f44191e4b419bfa09312984968bc15f284b16e8
ishiko <ishiko732@gmail.com>
2024-03-30 13:54:44 +0800

    add dev user

--- src/app/(auth)/api/auth/[...nextauth]/options.ts
+++ src/app/(auth)/api/auth/[...nextauth]/options.ts
@@ -1,20 +1,32 @@
 import type { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { GithubProfile } from "next-auth/providers/github";
 import { initUser } from "./init";
+import { Provider } from "next-auth/providers/index";
+import { UserCreatedRequired } from "@/types";
+import * as bcrypt from "bcrypt";
 
-export const options: NextAuthOptions = {
-  // debug: process.env.NODE_ENV !== "production",
-  providers: [
+function getProviders(): Provider[] {
+  const providers: Provider[] = [];
+
+  providers.push(
     GitHubProvider({
       async profile(profile: GithubProfile) {
         // console.log(profile);
-        const user = await initUser(profile);
+        const userCreatedRequired: UserCreatedRequired = {
+          name: !profile.name ? profile.login : profile.name,
+          email: profile.email ?? "",
+          oauthId: profile.id.toString(),
+          oauthType: "github",
+          password: "",
+        };
+        const user = await initUser(userCreatedRequired);
         const githubProfile = {
           ...profile,
-          name: profile.name ?? profile.login,
+          name: user.name,
           role:
-            profile.id === (Number(process.env.GITHUB_ADMIN_ID) ?? 62931549)
+            profile.id === (Number(process.env.GITHUB_ADMIN_ID) ?? 62931549) // ishiko732 GitHub_Id=62931549
               ? "admin"
               : "user",
           id: user.uid.toString(),
@@ -24,49 +36,69 @@ export const options: NextAuthOptions = {
       },
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
-      httpOptions:{
-        timeout:60000
-      }
-    }),
-    // CredentialsProvider({
-    //   id: "custom-login",
-    //   name: "Credentials",
-    //   credentials: {
-    //     username: {
-    //       label: "Username or email address",
-    //       type: "text",
-    //     },
-    //     password: {
-    //       label: "Password:",
-    //       type: "password",
-    //     },
-    //   },
-    //   async authorize(credentials) {
-    //     // This is where you need to retrieve user data
-    //     // to verify with credentials
-    //     // Docs: https://next-auth.js.org/configuration/providers/credentials
-    //     console.log(credentials);
-    //     const user = await prisma.user.findFirst({
-    //       where: {
-    //         OR: [
-    //           { name: credentials?.username ?? "" },
-    //           { email: credentials?.username ?? "" },
-    //         ],
-    //       },
-    //     });
-    //     console.log(user);
-    //     if (user && user.password === credentials?.password) {
-    //       return {
-    //         id: user.uid.toString(),
-    //         name: user.name,
-    //         image: "https://avatars.githubusercontent.com/u/62931549?v=4",
-    //         role: user.uid === 1 ? "admin" : "user",
-    //       };
-    //     }
-    //     return null;
-    //   },
-    // }),
-  ],
+      httpOptions: {
+        timeout: 60000,
+      },
+    })
+  );
+  if (process.env.NODE_ENV !== "production") {
+    providers.push(
+      CredentialsProvider({
+        id: "custom-login",
+        name: "Dev",
+        credentials: {
+          email: {
+            label: "[dev]email",
+            type: "email",
+            value: "test@gmail.com",
+          },
+          username: {
+            label: "[dev]username",
+            type: "text",
+            value: "DevUser",
+          },
+          password: {
+            label: "[dev]password",
+            type: "password",
+            value: "123",
+          },
+        },
+        async authorize(profile) {
+          console.log(profile);
+          if (!profile) {
+            return null;
+          }
+          // This is where you need to retrieve user data
+          // to verify with credentials
+          // Docs: https://next-auth.js.org/configuration/providers/credentials
+          const hashedPassword = await bcrypt.hash(profile.password, 10);
+          const userCreatedRequired: UserCreatedRequired = {
+            name: profile.username,
+            email: profile.email,
+            oauthId: "",
+            oauthType: "dev",
+            password: hashedPassword,
+          };
+          const user = await initUser(userCreatedRequired);
+          if (user && (await bcrypt.compare(profile.password, user.password))) {
+            return {
+              id: user.uid.toString(),
+              name: user.name,
+              image: "https://avatars.githubusercontent.com/u/96821265?v=4",
+              role: "admin",
+            };
+          }
+          return null;
+        },
+      })
+    );
+  }
+  return providers;
+}
+
+export const options: NextAuthOptions = {
+  // debug: process.env.NODE_ENV !== "production",
+  providers: getProviders(),
   callbacks: {
     // Ref: https://authjs.dev/guides/basics/role-based-access-control#persisting-the-role
     async jwt({ token, user }) {
@@ -79,10 +111,9 @@ export const options: NextAuthOptions = {
     // If you want to use the role in client components
     async session({ session, token }) {
       if (session?.user) {
-        session.user.role = token.role
-        if(token.sub)
-        session.user.id=token.sub
-      };
+        session.user.role = token.role;
+        if (token.sub) session.user.id = token.sub;
+      }
       return session;
     },
   },
