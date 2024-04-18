89e9f8e997b8812ae18e02d47c72a3a45673cba0
ishiko <ishiko732@gmail.com>
2024-04-18 19:50:58 +0800

    Fix/remove stability.toFixed(2)

--- src/components/schedule/DSR.tsx
+++ src/components/schedule/DSR.tsx
@@ -7,7 +7,7 @@ export default function DSRDisplay() {
     return DSR && !open && currentType === State.Review ? (
         <div className="flex justify-center opacity-15 flex-col text-left mx-auto">
             <div>{`D : ${DSR.D.toFixed(2)}`}</div>
-            <div>{`S : ${DSR.S.toFixed(2)}`}</div>
+            <div>{`S : ${DSR.S}`}</div>
             <div>{`R : ${DSR.R}`}</div>
         </div>
     ) : null
