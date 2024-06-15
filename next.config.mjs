9a63ce3a449e53c646ea0dd20b6f878908c13f03
ishiko <ishiko732@gmail.com>
2024-06-15 15:09:21 +0800

    refator/update deps

--- next.config.mjs
+++ next.config.mjs
@@ -50,6 +50,10 @@ const nextConfig = {
         ],
       },
     ];
-  }
+  },
+  generateBuildId: async () => {
+    // This could be anything, using the latest git hash
+    return process.env.GIT_HASH
+  },
 };
-module.exports = nextConfig;
+export default nextConfig;
