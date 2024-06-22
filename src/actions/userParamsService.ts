8aea7af26638216eb2bdc0626a46c1b022124b1e
ishiko <ishiko732@gmail.com>
2024-06-22 23:45:52 +0800

    update cache

--- src/actions/userParamsService.ts
+++ src/actions/userParamsService.ts
@@ -6,23 +6,24 @@ import {
   getFSRSParamsByUid,
   updateParameters,
 } from '@/lib/fsrs';
-import { revalidatePath, unstable_cache as cache } from 'next/cache';
+import { unstable_cache as cache, revalidateTag } from 'next/cache';
 type IRespose = {
   code: number;
   msg: string;
   data: null | ParametersType;
 };
 
-async function _getUserParams() {
+export async function getUserParams() {
   const uid = await getSessionUserId();
   if (!uid) {
     return { code: 401, msg: 'user not found.', data: null } as IRespose;
   }
+  const userCacheParams = getUserParams_cache(uid);
   try {
     return {
       code: 200,
       msg: 'success',
-      data: await getFSRSParamsByUid(uid),
+      data: await userCacheParams(),
     } as IRespose;
   } catch (e: any) {
     return { code: 500, msg: (e as Error).message, data: null } as IRespose;
@@ -31,9 +32,17 @@ async function _getUserParams() {
 
 // Ref: https://nextjs.org/docs/app/api-reference/functions/unstable_cache
 // Ref: https://www.reddit.com/r/nextjs/comments/18y6cw0/in_server_actions_how_to_cache_external_data_if/
-export const getUserParams = cache(_getUserParams, [
-  'actions/user-params-' + getSessionUserId(),
-]);
+// Ref: https://zenn.dev/frontendflat/articles/nextjs-cache-2024
+const getUserParams_cache = (uid: number) =>
+  cache(
+    async () => {
+      return await getFSRSParamsByUid(uid);
+    },
+    [`actions/user-params/${uid}`],
+    {
+      tags: [`actions/user-params/${uid}`],
+    }
+  );
 
 type ICommitUserParams = {
   request_retention: number;
@@ -54,7 +63,7 @@ export async function commitUserParams(data: ICommitUserParams) {
     ...data,
     uid,
   });
-  revalidatePath(`actions/user-params-${uid}`);
+  revalidateTag(`actions/user-params/${uid}`);
   return {
     code: 200,
     msg: 'success',
