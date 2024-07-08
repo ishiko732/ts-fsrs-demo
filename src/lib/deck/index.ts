2343fa7d14f6e74203f33f8c7691070da9dfa0e7
ishiko <ishiko732@gmail.com>
2024-07-09 00:27:15 +0800

    mv deck

--- src/lib/deck/index.ts
+++ src/lib/deck/index.ts
@@ -1,36 +1,7 @@
-import { Card, Note } from "@prisma/client";
-import { FSRS, FSRSParameters, RecordLog, date_scheduler, fsrs, generatorParameters } from "ts-fsrs";
-import prisma from "./prisma";
+import { date_scheduler, fsrs, generatorParameters } from "ts-fsrs";
+import prisma from "../prisma";
 import { State as PrismaState } from "@prisma/client";
-
-type SearchTodayMemoryContextPage = {
-    uid: number,
-    startTimestamp: number,
-    userNewCardlimit: number,
-    deckTodayLearnedcount: number,
-    page?: number,
-    ignoreCardIds?: number[]
-}
-
-
-abstract class IDeckService {
-    abstract getDeck(deckId: number): void;
-    abstract getAlgorithmParams(uid: number): Promise<FSRSParameters>;
-    abstract getAlgorithm(uid: number): Promise<FSRS>;
-    abstract getTodayMemoryContext(uid: number, timezone: string, hourOffset: number): Promise<DeckMemoryContext>;
-    abstract todayMemoryContextPage({ uid, startTimestamp, userNewCardlimit, deckTodayLearnedcount, page, ignoreCardIds }: SearchTodayMemoryContextPage): Promise<NoteMemoryContext>
-
-}
-
-abstract class INoteService {
-    abstract getNote(nid: number): Note;
-    abstract getCard(nid: number): Card;
-    abstract schduler(cid: number): void;
-    abstract undo(): void;
-    abstract rollback(): void;
-    abstract edit(nid: number): Note;
-    abstract previewRepeat(card: Card): RecordLog;
-}
+import { DeckMemoryContext, IDeckService, NoteMemoryContext, NoteMemoryState, SearchTodayMemoryContextPage } from "./type";
 
 
 // TODO: get deck notes
@@ -191,43 +162,7 @@ async function getDBAlgorithmParams(uid: number) {
     });
 }
 
-// deck
-export interface DeckMemoryState {
-    uid: number;
-    timezone: string;
-    startTimestamp: number;
-    nextTimestamp: number;
-    userNewCardlimit: number;
-    deckTodayLearnedcount: number;
-}
 
-export interface DeckMemoryContext extends DeckMemoryState {
-    noteContext: NoteMemoryContext;
-}
-
-export interface NoteMemoryState {
-    deckId: number;
-    noteId: number,
-    cardId: number,
-    due: number, // due timestamp
-}
-
-export interface NoteMemoryStatePage {
-    memoryState: NoteMemoryState[];
-    pageSize: number;
-    loadPage: number;
-    totalSize: number
-}
-
-type NoteMemoryContext = {
-    [key in PrismaState]: NoteMemoryStatePage;
-};
-
-// TODO
-enum NoteOrder  {
-    lastReview,
-    Difficulty
-}
 
 
 // note
---
+++ src/lib/deck/type.d.ts
@@ -0,0 +1,75 @@
+type SearchTodayMemoryContextPage = {
+  uid: number;
+  startTimestamp: number;
+  userNewCardlimit: number;
+  deckTodayLearnedcount: number;
+  page?: number;
+  ignoreCardIds?: number[];
+};
+
+abstract class IDeckService {
+  abstract getDeck(deckId: number): void;
+  abstract getAlgorithmParams(uid: number): Promise<FSRSParameters>;
+  abstract getAlgorithm(uid: number): Promise<FSRS>;
+  abstract getTodayMemoryContext(
+    uid: number,
+    timezone: string,
+    hourOffset: number
+  ): Promise<DeckMemoryContext>;
+  abstract todayMemoryContextPage({
+    uid,
+    startTimestamp,
+    userNewCardlimit,
+    deckTodayLearnedcount,
+    page,
+    ignoreCardIds,
+  }: SearchTodayMemoryContextPage): Promise<NoteMemoryContext>;
+}
+
+abstract class INoteService {
+  abstract getNote(nid: number): Note;
+  abstract getCard(nid: number): Card;
+  abstract schduler(cid: number): void;
+  abstract undo(): void;
+  abstract rollback(): void;
+  abstract edit(nid: number): Note;
+  abstract previewRepeat(card: Card): RecordLog;
+}
+
+// deck
+export interface DeckMemoryState {
+  uid: number;
+  timezone: string;
+  startTimestamp: number;
+  nextTimestamp: number;
+  userNewCardlimit: number;
+  deckTodayLearnedcount: number;
+}
+
+export interface DeckMemoryContext extends DeckMemoryState {
+  noteContext: NoteMemoryContext;
+}
+
+export interface NoteMemoryState {
+  deckId: number;
+  noteId: number;
+  cardId: number;
+  due: number; // due timestamp
+}
+
+export interface NoteMemoryStatePage {
+  memoryState: NoteMemoryState[];
+  pageSize: number;
+  loadPage: number;
+  totalSize: number;
+}
+
+type NoteMemoryContext = {
+  [key in PrismaState]: NoteMemoryStatePage;
+};
+
+// TODO
+enum NoteOrder {
+  lastReview,
+  Difficulty,
+}
