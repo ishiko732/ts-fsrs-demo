e9159225eb28bded7d32417189fc0d83d1a7cff9
ishiko <ishiko@influx.io>
2025-03-07 19:00:34 +0900

    fix

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,7 +1,8 @@
 import { Env } from '@server/bindings'
-import AuthApp from '@server/controllers/auth'
+import UserApp from '@server/controllers/users'
 import { InitGlobalMiddlewares } from '@server/middlewares/global'
 import { Hono } from 'hono'
+import { showRoutes } from 'hono/dev'
 import { handle } from 'hono/vercel'
 
 export const runtime = 'nodejs'
@@ -12,8 +13,12 @@ const routes = app
   .notFound(async (c) => {
     return c.json({ error: 'Not found' }, 404)
   })
-  .route('/auth', AuthApp)
+  .route('/users', UserApp)
 
 const handler = handle(app)
+
+if (process.env.NODE_ENV === 'development') {
+  showRoutes(app)
+}
 export { handler as DELETE, handler as GET, handler as POST, handler as PUT }
 export type AppType = typeof routes
