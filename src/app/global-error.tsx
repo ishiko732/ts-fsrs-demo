46c9477ff48d6a8e63fbcfbe92838e4971ab1669
ishiko <ishiko@influx.io>
2025-04-02 18:37:19 +0900

    remove sentry

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,13 +1,12 @@
-"use client";
+'use client'
 
-import * as Sentry from "@sentry/nextjs";
-import NextError from "next/error";
-import { useEffect } from "react";
+import NextError from 'next/error'
+import { useEffect } from 'react'
 
 export default function GlobalError({ error }: { error: Error & { digest?: string } }) {
   useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
+    console.error(error)
+  }, [error])
 
   return (
     <html>
@@ -19,5 +18,5 @@ export default function GlobalError({ error }: { error: Error & { digest?: strin
         <NextError statusCode={0} />
       </body>
     </html>
-  );
+  )
 }
