ac0e64d7032af13beaf4af872ed69b41dfcab0b8
ishiko <ishiko732@gmail.com>
2025-05-10 15:07:49 +0800

    fix migrate

--- src/server/migrations/logs/20250510_learning_steps.ts
+++ src/server/migrations/logs/20250510_learning_steps.ts
@@ -4,14 +4,17 @@ import { type Kysely, sql } from 'kysely'
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .alterTable('cards')
-    .addColumn('learningSteps', 'integer', (col) => col.notNull().defaultTo(0))
+    .addColumn('learning_steps', 'integer', (col) => col.notNull().defaultTo(0))
     .execute()
 
   await db.schema
     .alterTable('revlog')
-    .addColumn('learningSteps', 'integer', (col) => col.notNull().defaultTo(0))
+    .addColumn('learning_steps', 'integer', (col) => col.notNull().defaultTo(0))
     .execute()
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function down(db: Kysely<any>): Promise<void> {}
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.alterTable('cards').dropColumn('learning_steps').execute()
+
+  await db.schema.alterTable('revlog').dropColumn('learning_steps').execute()
+}
